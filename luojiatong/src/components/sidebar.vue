cbde4c21567e1c180a26d0f5ebdea19a0cfb705f
Airhead42 <starrysea42@gmail.com>
2024-06-02 20:44:36 +0800

    修复：侧栏折叠状态下，新建日程的窗口显示异常的问题

--- luojiatong/src/components/sidebar.vue
+++ luojiatong/src/components/sidebar.vue
@@ -31,10 +31,10 @@
             </el-menu-item-group>
             <el-menu-item-group title="待办事项">
                 <el-menu-item index="2-3">
-                    <!-- 折叠状态下点这个会有问题 -->
                     <el-icon><Plus /></el-icon>
-                    <createNewEvent />
-                    <!-- <span>新建日程</span> -->
+                    <span @click="createNewEvent = true">
+                        新建日程
+                    </span>
                 </el-menu-item>
             </el-menu-item-group>
         </el-sub-menu>
@@ -74,25 +74,96 @@
         </el-sub-menu>
         
     </el-menu>
+
+    <!-- 新建日程 -->
+    <el-dialog v-model="createNewEvent" title="新建日程" width="500">
+        <el-form :model="form" label-width="auto" style="max-width: 480px">
+            <el-form-item label="名称">
+                <el-input v-model="form.name" />
+            </el-form-item>
+
+            <el-form-item label="时间">
+                <el-col :span="11">
+                    <el-date-picker v-model="form.startTime" type="datetime" placeholder="开始时间"
+                        format="YYYY-MM-DD HH:mm:ss" value-format="YYYY-MM-DD HH:mm:ss" style="width: 100%" />
+                </el-col>
+                <el-col :span="2" class="text-center">
+                    <span class="text-gray-500">-</span>
+                </el-col>
+                <el-col :span="11">
+                    <el-date-picker v-model="form.endTime" type="datetime" placeholder="结束时间"
+                        format="YYYY-MM-DD HH:mm:ss" value-format="YYYY-MM-DD HH:mm:ss" style="width: 100%" />
+                </el-col>
+            </el-form-item>
+
+            <el-form-item label="类型">
+                <el-select v-model="form.type" @change="$forceUpdate()">
+                    <el-option label="课程" value="1" />
+                    <el-option label="运动" value="2" />
+                    <el-option label="图书馆" value="3" />
+                    <el-option label="其他" value="4" />
+                </el-select>
+            </el-form-item>
+
+            <el-form-item label="地点">
+                <el-input v-model="form.place" />
+            </el-form-item>
+
+            <el-form-item label="备注">
+                <el-input v-model="form.content" type="textarea" />
+            </el-form-item>
+        </el-form>
+
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button type="primary" @click="onSubmit(form)">Create</el-button>
+                <el-button @click="createNewEvent = false">Cancel</el-button>
+            </div>
+        </template>
+    </el-dialog>
 </template>
 
 <script setup>
     import { reactive, ref, toRefs, onMounted  } from 'vue'
-    import createNewEvent from '../components/createNewEvent.vue'
     import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-    // 菜单部分
-    const isCollapse = ref(true)
-        const collapseNav = () => {
-            isCollapse.value = !isCollapse.value;
-        }
+    // 新建日程
+    const createNewEvent = ref(false)
 
-        const handleOpen = (key, keyPath) => {
-        console.log(key, keyPath)
-        }
-        const handleClose = (key, keyPath) => {
-        console.log(key, keyPath)
-        }
+    //增加事务模型
+    const form = reactive({
+        type: 0,
+        name: "",
+        place: "",
+        content: "",
+        startTime: "",
+        endTime: ""
+    })
+
+    const onSubmit = async (form) => {
+        form.startTime = form.startTime.split(" ").join("T");
+        form.endTime= form.endTime.split(" ").join("T");
+        console.log(form)
+        //调用接口
+
+        let result = await AddAffair(form);
+        console.log(result)
+
+        ElMessage.success(result.msg ? result.msg : '添加成功');
+    }
+
+    // 处理菜单折叠
+    const isCollapse = ref(true)
+    const collapseNav = () => {
+        isCollapse.value = !isCollapse.value;
+    }
+
+    const handleOpen = (key, keyPath) => {
+    console.log(key, keyPath)
+    }
+    const handleClose = (key, keyPath) => {
+    console.log(key, keyPath)
+    }
 </script>
 
 <style scoped>
