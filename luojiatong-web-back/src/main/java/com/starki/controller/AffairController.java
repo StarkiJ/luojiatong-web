889697ea22b36779e7c5ec165275e6fbea356437
StarkiJ <132117607+StarkiJ@users.noreply.github.com>
2024-06-09 20:09:49 +0800

    Update AffairController.java

--- luojiatong-web-back/src/main/java/com/starki/controller/AffairController.java
+++ luojiatong-web-back/src/main/java/com/starki/controller/AffairController.java
@@ -43,8 +43,12 @@ public class AffairController {
                        String place, String content,
                        @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDateTime startTime,
                        @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDateTime endTime) {
+        long msg = System.currentTimeMillis();//开始时间
         log.info("条件查询事务：{},{},{},{},{},{}", type, name, place, content, startTime, endTime);
         List<Affair> affairList = affairService.list(type, name, place, content, startTime, endTime);
+        log.info("所用时间: ");
+        String use = Long.toString(System.currentTimeMillis() - msg);//结束时间
+        log.info(use);
         return Result.success(affairList);
     }
 
@@ -56,44 +60,64 @@ public class AffairController {
      */
     @PostMapping
     public Result add(@RequestBody Affair affair) {
+        long msg = System.currentTimeMillis();
         log.info("新增事务, 参数: {}", affair);
         affairService.add(affair);
+
+        log.info("所用时间: ");
+        String use = Long.toString(System.currentTimeMillis() - msg);
+        log.info(use);
         return Result.success();
     }
 
     /**
      * 删除事务
+     *
      * @param ids
      * @return
      */
     @DeleteMapping("/{ids}")
     public Result delete(@PathVariable List<Integer> ids) {
+        long msg = System.currentTimeMillis();
         log.info("删除事务, ids: {}", ids);
         affairService.delete(ids);
+        log.info("所用时间: ");
+        String use = Long.toString(System.currentTimeMillis() - msg);
+        log.info(use);
         return Result.success();
     }
 
     /**
      * 修改事务
+     *
      * @param affair
      * @return
      */
     @PutMapping
     public Result update(@RequestBody Affair affair) {
+        long msg = System.currentTimeMillis();
         log.info("修改事务, 参数: {}", affair);
         affairService.update(affair);
+        log.info("所用时间: ");
+        String use = Long.toString(System.currentTimeMillis() - msg);
+        log.info(use);
         return Result.success();
     }
 
     /**
      * 删除课程
+     *
      * @param names
      * @return
      */
     @DeleteMapping("/course/{names}")
     public Result deleteCourse(@PathVariable List<String> names) {
+        long msg = System.currentTimeMillis();
         log.info("删除课程, names: {}", names);
         affairService.deleteCourse(names);
+        log.info("所用时间: ");
+        String use = Long.toString(System.currentTimeMillis() - msg);
+        log.info(use);
         return Result.success();
     }
 }
